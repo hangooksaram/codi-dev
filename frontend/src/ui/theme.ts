575e4ddd8348d8f4a96d79614db6f550e82b6fc6
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-07-03 22:41:02 +0900

    FEAT:Theme colors 추가

---
+++ frontend/src/ui/theme.ts
@@ -0,0 +1,17 @@
+import { ThemeProvider } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const theme = {
+  colors: {
+    black: "#161718",
+    secondary: "#29405d",
+    primary: "#209eff",
+    poinst: "#ffc64c",
+    notice: "#e92e22",
+    gray: "#a8a9af",
+    lightGray: "#c8c9d0",
+    lighterGray: "#eeeff2",
+    pointBg: "#ecf1f6",
+    white: "#fcfcfc",
+  },
+};
