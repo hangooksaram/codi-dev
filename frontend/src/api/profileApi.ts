0e388c3e75c90f8c49fed9cc560cc0fb4258ace1
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-07-10 09:49:32 +0900

    Revert "FEAT:Flex 컴포넌트 수정, ProfileCard 컴포넌트 추가"

--- frontend/src/api/profileApi.ts
+++
@@ -1,7 +0,0 @@
-import { Profile } from "@/types/profile";
-import axios from "axios";
-
-const registerProfile = async (profile: Profile) =>
-  (await axios.post("", profile)).data;
-
-export { registerProfile };
--- frontend/src/api/signApi.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { SignUpData, SignInData } from "@/types/sign";
-const signUp = async (signUpData: SignUpData) =>
-  (await axios.post("", signUpData)).data;
-const signIn = async (signInData: SignInData) => {
-  return (await axios.post("", signInData)).data;
-};
-
-export { signIn, signUp };
