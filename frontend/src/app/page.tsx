331a71a1f7fbae34e375d8024aa8513e474df3a7
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-07-23 18:46:16 +0900

    FEAT:페이지 추가 및 수정
    1. signup, profileForm, mentorApplyForm 등 폼페이지에 Formik, useRestForm 커스텀 훅 적용
    2. 디자인 작업

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,93 @@
 "use client";
 
-import ProfileCard from "@/component/Profile/ProfileCard";
+import Recommendation from "@/component/Mentors/Recommendation";
+import useGetMentorsQuery, { GET_MENTORS_KEY } from "@/queries/mentorQuery";
+import { Mentor } from "@/types/mentor";
+import LandingBanner from "../../public/images/landing-banner.png";
+import styled from "@emotion/styled";
+import { backgroundImage } from "@/ui/atoms/BackgroundImage/BackgroundImage";
+import Typography from "@/ui/atoms/Typography/Typography";
+import Button from "@/ui/atoms/Button/Button";
+import theme from "@/ui/theme";
+import { css } from "@emotion/css";
 
-export default function Home() {
-  return <main></main>;
-}
+import { useRouter } from "next/navigation";
+import { StyledImage } from "@/ui/atoms/Image/StyledImage";
+import FlexBox from "@/ui/atoms/Layout/FlexBox";
+import { isUser } from "@/utils/tempUser";
+
+const MainLandingPage = () => {
+  const { data, isLoading, isSuccess } = useGetMentorsQuery({
+    page: 1,
+    size: 10,
+  });
+  const router = useRouter();
+  const mentors = data?.data! as Mentor[];
+  const recommendationMentors = mentors?.filter((mentor, index) => index < 4);
+  return (
+    isSuccess && (
+      <MainContainer>
+        <FlexBox direction="column" rowGap="20px">
+          <MainBanner src={LandingBanner.src}>
+            <div
+              className={css`
+                width: 460px;
+                word-break: break-word;
+                margin-bottom: 10px;
+              `}
+            >
+              <Typography
+                variant="h1"
+                size={theme.fonts.size.xl}
+                align="center"
+              >
+                SHARE THE SAME EXPERIENCE
+              </Typography>
+            </div>
+            <Typography variant="div" {...{ marginBottom: "40px" }}>
+              나와 같은 어려움을 겪고 있는 멘티의 멘토가 되어주세요.
+            </Typography>
+            <Button
+              onClick={() => {
+                if (isUser()) {
+                  router.push("/mentorApplyForm");
+                  return;
+                }
+                router.push("/signup");
+              }}
+              variant="default"
+            >
+              멘토 신청하러 가기
+            </Button>
+          </MainBanner>
+          <Recommendation mentors={recommendationMentors} />
+          <StyledImage
+            width="100%"
+            height="2300px"
+            {...{ minHeight: "1900px" }}
+            src="/images/main-landing-bottom.png"
+            alt="main-landing-bottom"
+          />
+        </FlexBox>
+      </MainContainer>
+    )
+  );
+};
+
+const MainContainer = styled.main`
+  width: "100%";
+  background-color: ${theme.colors.background};
+`;
+
+const MainBanner = styled.div(({ src }: { src: string }) => ({
+  ...backgroundImage(src),
+  width: "100%",
+  height: "auto",
+  padding: "10%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+export default MainLandingPage;
