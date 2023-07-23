e1b66c606f0b9a3d596405ff075156e30e06fd1e
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-07-23 18:44:45 +0900

    FEAT:AppBar 스타일 수정

--- frontend/src/component/NavBar/AppBar.tsx
+++ frontend/src/component/NavBar/AppBar.tsx
@@ -1,51 +1,101 @@
 import theme from "@/ui/theme";
 import styled from "@emotion/styled";
-import Logo from "../../../public/images/logo-primary.svg";
+import Logo from "../../../public/icons/logo-primary.svg";
 import FlexBox from "@/ui/atoms/Layout/FlexBox";
 import Image from "next/image";
 import StyledLink from "@/ui/atoms/Button/Link";
 import Alarm from "../../../public/icons/alarm.svg";
 import Button from "@/ui/atoms/Button/Button";
-
+import { useParams, usePathname, useRouter } from "next/navigation";
+import { isUser } from "@/utils/tempUser";
+import Typography from "@/ui/atoms/Typography/Typography";
+import Link from "next/link";
+import Profile from "../../../public/icons/profile.svg";
 const StyledAppBar = styled.nav`
   width: 100%;
-  position: fixed;
+  position: sticky;
+  top: 0;
   z-index: 1;
-  height: 61px;
+  height: 59px;
   background-color: ${theme.colors.white};
   border-bottom: 1px solid ${theme.colors.gray.main};
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const AppBarContent = styled(FlexBox)`
-  width: 80%;
+  width: 90%;
   align-items: center;
-  margin: 10px auto;
+
   justify-content: space-between;
 `;
 
 const AppBarProfile = styled.div`
   width: 42px;
   height: 42px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.gray.light};
   border-radius: 100%;
-  background-color: red;
 `;
 
 const AppBar = () => {
+  const path = usePathname();
+  const router = useRouter();
+
+  if (path === "/signin/") return;
   return (
     <StyledAppBar>
       <AppBarContent>
         <FlexBox justifyContent="flex-start" columnGap="90px">
-          <Logo width="108px" height="26px" />
-          <StyledLink href="/">멘토 페이지</StyledLink>
+          <Link href={"/"}>
+            <Logo width="108px" height="26px" />
+          </Link>
+          <StyledLink href="/mentors">멘토 페이지</StyledLink>
           <StyledLink href="/">마이코디</StyledLink>
         </FlexBox>
-        <FlexBox justifyContent="flex-end" columnGap="30px">
-          <Alarm />
-          <AppBarProfile />
-          <Button size="small" variant="default" color={theme.colors.primary}>
-            멘토 신청
-          </Button>
-        </FlexBox>
+        {isUser() ? (
+          <FlexBox justifyContent="flex-end" columnGap="30px">
+            <Alarm />
+            <AppBarProfile>
+              <Profile />
+            </AppBarProfile>
+            <Button
+              size="small"
+              variant="default"
+              color={theme.colors.primary}
+              {...{ height: "39px" }}
+              onClick={() => router.push("/mentorApplyForm")}
+            >
+              멘토 신청
+            </Button>
+          </FlexBox>
+        ) : (
+          <FlexBox justifyContent="flex-end" columnGap="30px">
+            <StyledLink href="/signup">
+              아이디가 없으신가요?
+              <Typography
+                variant="span"
+                size={theme.fonts.size.sm}
+                weight={theme.fonts.weight.bold}
+                {...{ marginLeft: "4px" }}
+              >
+                회원가입
+              </Typography>
+            </StyledLink>
+            <Button
+              size="small"
+              variant="default"
+              color={theme.colors.primary}
+              {...{ height: "39px" }}
+              onClick={() => router.push("/signin")}
+            >
+              로그인
+            </Button>
+          </FlexBox>
+        )}
       </AppBarContent>
     </StyledAppBar>
   );
