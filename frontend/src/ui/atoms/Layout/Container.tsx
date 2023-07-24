9620656d3b187c9f19cfb76583bdd7b48a690d3e
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-07-24 17:06:23 +0900

    FEAT: Container width, Text 관련 컴포넌트 min-width 속성 수정
    1. Container 90% -> 69% (디자인 width 값 반영)
    2. Link, 폼 인풋 라벨 에 min-width 속성 추가

--- frontend/src/ui/atoms/Layout/Container.tsx
+++ frontend/src/ui/atoms/Layout/Container.tsx
@@ -2,12 +2,12 @@ import theme from "@/ui/theme";
 import styled from "@emotion/styled";
 
 const Container = styled.main(({ width }: { width?: string }) => ({
-  width: width ?? "90%",
+  width: width ?? "69%",
   margin: "0 auto",
 }));
 
 export const FormContainer = styled(Container)`
-  width: 50%;
+  width: 68%;
   max-width: 640px;
   padding-bottom: 80px;
 `;
