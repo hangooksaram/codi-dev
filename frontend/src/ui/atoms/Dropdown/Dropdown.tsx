22061c268c6963ecd1c475571660ef71f919a8ec
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-07-27 10:04:34 +0900

    CHORE:Dropdown 컴포넌트 디렉토리구조 수정
    1. 각 타입 (List, Grid) 에 따른 뷰를 각 파일로 관리 -> 하나의 파일 안으로 묶음. 이유는 현재 /ui/atoms 내 구조는 해당 컴포넌트 이름 (Input, Button...) 으로 디렉토리가 있고 그 밑에 그것과 관련된 다른 종류들의 컴포넌트들이 있기 때문에, 하나의 Dropdown 을 구성하는 하위 컴포넌트가 들어가기에는 위계가 적합하지 않은 듯함.

--- frontend/src/ui/atoms/Dropdown/Dropdown.tsx
+++ frontend/src/ui/atoms/Dropdown/Dropdown.tsx
@@ -4,25 +4,9 @@ import theme from "@/ui/theme";
 import { Dropdown } from "@/ui/ui";
 import styled from "@emotion/styled";
 import OpenDropdown from "@icons/common/expand-open.svg";
-import DropdownListContent from "./DropdownListContent";
-import DropdownGridContent from "./DropdownGridContent";
-
-const DropDownListContainer = styled.div(({ width }: { width?: string }) => ({
-  width: width,
-  minWidth: "fit-content",
-  position: "relative",
-}));
-
-const DropdownButton = styled(Button)(
-  ({ invalid }: { invalid: boolean | undefined }) => ({
-    justifyContent: "space-between",
-    border: "1px solid",
-    borderColor:
-      invalid === true && invalid !== undefined
-        ? theme.colors.error
-        : theme.colors.gray.main,
-  })
-);
+import FlexBox from "../Layout/FlexBox";
+import Checkbox from "../Checkbox/Checkbox";
+import Card from "../Card/Card";
 
 const Dropdown = ({
   width,
@@ -69,4 +53,115 @@ const Dropdown = ({
   );
 };
 
+const DropdownGridContent = ({
+  categories,
+  handleClick,
+}: {
+  categories: string[] | number[];
+  handleClick: Function;
+}) => {
+  return (
+    <DropdownGridCard>
+      <FlexBox
+        justifyContent="flex-start"
+        width="100%"
+        isWrap
+        columnGap="80px"
+        rowGap="22px"
+      >
+        {categories.map((category, index) => (
+          <Checkbox
+            width="20%"
+            label={category}
+            key={`${index}-${category}`}
+            handleClick={handleClick}
+          />
+        ))}
+      </FlexBox>
+    </DropdownGridCard>
+  );
+};
+
+const DropdownListContent = ({
+  categories,
+  handleClick,
+}: {
+  categories: string[] | number[];
+  handleClick: Function;
+}) => {
+  return (
+    <DropDownList width="100%">
+      {categories.map((category, index) => (
+        <div key={`${index}-${category}`}>
+          <DropdownItem onClick={() => handleClick(category)}>
+            {category}
+          </DropdownItem>
+          {index < categories.length - 1 && <Divider />}
+        </div>
+      ))}
+    </DropDownList>
+  );
+};
+
+const DropDownListContainer = styled.div(({ width }: { width?: string }) => ({
+  width: width,
+  minWidth: "fit-content",
+  position: "relative",
+}));
+
+const DropdownButton = styled(Button)(
+  ({ invalid }: { invalid: boolean | undefined }) => ({
+    justifyContent: "space-between",
+    border: "1px solid",
+    borderColor:
+      invalid === true && invalid !== undefined
+        ? theme.colors.error
+        : theme.colors.gray.main,
+  })
+);
+
+const DropDownList = styled.ul(({ width }: { width?: string }) => ({
+  width: width ?? "100%",
+  minWidth: "fit-content",
+  maxHeight: "250px",
+  overflow: "auto",
+  position: "absolute",
+  zIndex: 1,
+  top: "70px",
+  padding: "0px 10px",
+  backgroundColor: theme.colors.white,
+  borderRadius: "10px",
+  listStyle: "none",
+  border: `1px solid ${theme.colors.gray.main}`,
+}));
+
+const DropdownGridCard = styled(Card)(() => ({
+  width: "100%",
+  height: "auto",
+  minWidth: "fit-content",
+  position: "absolute",
+  zIndex: 1,
+  top: "70px",
+  padding: "40px",
+}));
+
+const DropdownItem = styled.li`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0px 21.5px;
+  min-width: 130px;
+  color: ${theme.colors.black};
+  font-size: ${theme.fonts.size.sm};
+  font-weight: ${theme.fonts.weight.regular};
+  background-color: ${theme.colors.white};
+  cursor: pointer;
+`;
+
+const Divider = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  height: 2px;
+  background-color: ${theme.colors.background};
+`;
 export default Dropdown;
