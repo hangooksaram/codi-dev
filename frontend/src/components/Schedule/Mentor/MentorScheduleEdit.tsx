32f5b396fd8b83ddf51c73d7ba756b72bb876930
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-08-05 18:33:08 +0900

    FEAT:스케줄 (멘토링 가능 시간) 관련 컴포넌트 추가

---
+++ frontend/src/components/Schedule/Mentor/MentorScheduleEdit.tsx
@@ -0,0 +1,79 @@
+import { SCHEDULE_TIME_TABLE } from "@/constants";
+import Button from "@/ui/atoms/Button";
+import Card from "@/ui/atoms/Card";
+import Chip from "@/ui/atoms/Chip";
+import FlexBox from "@/ui/atoms/FlexBox";
+import Typography from "@/ui/atoms/Typography";
+import theme from "@/ui/theme";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+const MentorScheduleEdit = ({
+  date,
+  toggleEditState,
+}: {
+  date?: string;
+  toggleEditState: () => void;
+}) => {
+  const [selecteds, setSelecteds] = useState<string[]>([]);
+
+  const selected = (time: string) => {
+    return selecteds.includes(time);
+  };
+
+  const handleClickTime = (time: string) => {
+    if (selecteds.includes(time)) {
+      setSelecteds((prev) => prev.filter((prevTime) => prevTime !== time));
+      return;
+    }
+    setSelecteds([...selecteds, time]);
+  };
+
+  const patchMentorSchedule = () => {
+    toggleEditState();
+  };
+
+  return (
+    <Card padding="45px">
+      <FlexBox direction="column" rowGap="23px">
+        <FlexBox justifyContent="flex-start" columnGap="20px">
+          <Typography variant="div">시간 선택</Typography>
+          <Button
+            onClick={() => setSelecteds([])}
+            variant="default"
+            outline
+            size="small"
+            color={theme.colors.white}
+          >
+            전체 삭제
+          </Button>
+        </FlexBox>
+
+        <FlexBox
+          isWrap
+          rowGap="15px"
+          columnGap="15px"
+          justifyContent="flex-start"
+        >
+          {SCHEDULE_TIME_TABLE.map((time, index) => (
+            <Chip
+              onClick={() => handleClickTime(time)}
+              color={selected(time) ? theme.colors.primary : theme.colors.white}
+              fontColor={
+                selected(time) ? theme.colors.white : theme.colors.gray.main
+              }
+              outline
+              key={index}
+            >
+              {time}
+            </Chip>
+          ))}
+        </FlexBox>
+        <Button onClick={patchMentorSchedule} size="small" variant="default">
+          변경내용저장
+        </Button>
+      </FlexBox>
+    </Card>
+  );
+};
+
+export default MentorScheduleEdit;
