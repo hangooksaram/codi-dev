992a16ce7f3b3941487a7cafba17ee156431cca8
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-08-05 16:26:04 +0900

    FEAT:Chip color, fontColor props 추가

--- frontend/src/ui/atoms/Chip.tsx
+++ frontend/src/ui/atoms/Chip.tsx
@@ -1,18 +1,31 @@
 import styled from "@emotion/styled";
 import theme from "../theme";
 
-const Chip = styled.div(({ size }: { size?: "small" }) => ({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  height: size === "small" ? "31px" : "39px",
-  fontSize: theme.fonts.size.xs,
-  backgroundColor: theme.colors.background,
-  color: theme.colors.primary,
-  padding: "0px 20px",
-  width: "fit-content",
-  borderRadius: "100px",
-  cursor: "default",
-}));
+const Chip = styled.div(
+  ({
+    size,
+    color,
+    fontColor,
+    outline,
+  }: {
+    size?: "small";
+    color?: string;
+    fontColor?: string;
+    outline?: boolean;
+  }) => ({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: size === "small" ? "31px" : "39px",
+    fontSize: theme.fonts.size.xs,
+    backgroundColor: color ?? theme.colors.background,
+    color: fontColor ?? theme.colors.primary,
+    padding: "0px 20px",
+    width: "fit-content",
+    borderRadius: "100px",
+    cursor: "default",
+    border: outline ? `1px solid ${theme.colors.gray.main}` : "none",
+  })
+);
 
 export default Chip;
