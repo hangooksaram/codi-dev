d724190da4961b3b2dba945ff5d745975561e836
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-09-06 17:43:58 +0900

    FIX:멘토링 캘린더에서 useDailyMentoringQuery refetch 안하도록 수정₩
    1. refetch 를 명시적으로 사용하는 것이아닌, queryKey 를 상태로 수정하는 것으로 수정

--- frontend/src/queries/mentoring/commonMentoringQuery.ts
+++ frontend/src/queries/mentoring/commonMentoringQuery.ts
@@ -23,11 +23,11 @@ export const useDailyMentoringsQuery = ({
   profileId,
   mentorId,
 }: GetDailyMentoringsParams) => {
-  const queyKey = profileId
+  const querykey = profileId
     ? GET_MENTEE_DAILY_MENTORINGS_KEY
     : GET_MENTOR_DAILY_MENTORINGS_KEY;
   return useQuery(
-    queyKey.concat(date),
+    querykey.concat(date),
     () => getDailyMentorings({ profileId, mentorId, date }),
     {
       enabled: mentorId !== undefined || profileId !== undefined,
@@ -42,12 +42,12 @@ export const useMonthlyMentoringsQuery = ({
   profileId,
   mentorId,
 }: GetMonthlyMentoringsParams) => {
-  const queyKey = profileId
+  const querykey = profileId
     ? GET_MENTEE_MONTHLY_MENTORINGS_KEY
     : GET_MENTOR_MONTHLY_MENTORINGS_KEY;
 
   return useQuery<GetMonthlyMentoringsResponse>(
-    queyKey.concat(month),
+    querykey.concat(month),
     () => getMonthlyMentorings({ profileId, mentorId, month }),
     {
       enabled:
