1a8389a2a2afa44bcee7ed32390f2634b80b92df
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-09-27 23:51:59 +0900

    FEAT:mentorQuery refetch 로 검색 -> key 변화로 검색 트리거 되도록 수정

--- frontend/src/components/Mentor/Mentors/index.tsx
+++ frontend/src/components/Mentor/Mentors/index.tsx
@@ -6,12 +6,18 @@ import Card from "@/ui/atoms/Card";
 import Typography from "@/ui/atoms/Typography";
 
 const Mentors = () => {
-  const { query, setQuery, mentors, isSuccess, refetch } = useGetMentorsQuery();
+  const { query, setQuery, mentors, isSuccess, refetch, setSearched } =
+    useGetMentorsQuery();
 
   return (
     isSuccess && (
       <>
-        <MentorSearch query={query} setQuery={setQuery} refetch={refetch} />
+        <MentorSearch
+          query={query}
+          setQuery={setQuery}
+          refetch={refetch}
+          setSearched={setSearched}
+        />
         <div className={css({ marginTop: "40px" })}>
           <MentorList mentors={mentors} />
         </div>
