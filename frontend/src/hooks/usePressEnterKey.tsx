d90722063438dc50b01b26d6201c947d9844f0d1
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-27 15:26:39 +0900

    FEAT:로그인 시 "Enter" 키 로 가능하도록 수정
    * usePressEnterKey 커스텀 훅 추가
       - 클릭 하려는 버튼 의 ref 를 의존성으로 받아, "keydown" 이벤트에서 버튼 을 클릭 하는 이벤트를 추가

---
+++ frontend/src/hooks/usePressEnterKey.tsx
@@ -0,0 +1,18 @@
+import { Ref, RefObject, useEffect } from "react";
+
+const usePressEnterKey = (element: RefObject<HTMLButtonElement>) => {
+  const clickWhenEnterIsPressed = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      element!.current!.click();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", clickWhenEnterIsPressed);
+    return () => {
+      document.removeEventListener("keydown", clickWhenEnterIsPressed);
+    };
+  }, []);
+};
+
+export default usePressEnterKey;
