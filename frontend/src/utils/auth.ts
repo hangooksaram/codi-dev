5b5e507f70a00f1e7a3ea74ae788c6373ac21b16
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-12-01 16:10:25 +0900

    FEAT:로그인 응답 헤더의 토큰을 로컬 스토리지에 저장하는 로직 수정
    * Refresh 토큰 추가

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,22 +1,27 @@
+import { Token } from "@/types/user";
 import { AxiosResponseHeaders } from "axios";
-const authorizationKey = "authorization";
+const accessTokenKeyName = "authorization";
+const refreshTokenKeyName = "refresh";
+
 const localAuthorizationKey = "signin-token";
 
-export const getToken = (headers: AxiosResponseHeaders): string => {
-  if (authorizationKey in headers) {
-    return headers[authorizationKey] as string;
+export const getToken = (headers: AxiosResponseHeaders): Token | undefined => {
+  if (accessTokenKeyName in headers && refreshTokenKeyName in headers) {
+    return {
+      access: { value: headers[accessTokenKeyName] as string, expiredTime: 0 },
+      refresh: {
+        value: headers[refreshTokenKeyName] as string,
+        expiredTime: 0,
+      },
+    };
   }
-  return "token is not received";
+  return undefined;
 };
 
-export const setTokenToLocalStorage = (token: string) => {
-  // if (localStorage.getItem(localAuthorizationKey)) {
-  //   return;
-  // }
-
-  localStorage.setItem(localAuthorizationKey, token);
+export const setTokenToLocalStorage = (token: Token) => {
+  localStorage.setItem(localAuthorizationKey, JSON.stringify(token));
 };
 
-export const getTokenFormLocalStorage = () => {
-  return localStorage.getItem(localAuthorizationKey)!;
+export const getTokenFormLocalStorage = (): Token | undefined => {
+  return JSON.parse(localStorage.getItem(localAuthorizationKey)!);
 };
