107148b3c82fe15fa799276990327ccde3f4495d
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-12-19 15:15:43 +0900

    FEAT:RootLayoutWrapper 컴포넌트 추가
    * root layout 은 server component 이어야 하기 때문에, client 로직을 담을 수 있는 클라이언트 컴포넌트 생성

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,37 +1,11 @@
-"use client";
-import React, { ReactNode, useEffect } from "react";
-import "./globals.css";
-import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { store } from "@/store/store";
-import AccessibilityLayout from "@/components/Layout/AccessibilityLayout";
-import { ThemeProvider } from "@emotion/react";
-import theme from "@/ui/theme";
-import AppBar from "@/components/NavBar/AppBar";
-import Floating from "@/components/Accessibility/Floating";
-import AuthContainer from "@/components/Container/AuthContainer";
-/**create new client */
-const queryClient = new QueryClient();
+import RootLayoutWrapper from "@/components/RootLayoutWrapper";
+import { ReactNode } from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
       <body suppressHydrationWarning={true}>
-        <ThemeProvider theme={theme}>
-          <Provider store={store}>
-            <QueryClientProvider client={queryClient}>
-              <AccessibilityLayout>
-                <AppBar />
-                {children}
-                <Floating />
-              </AccessibilityLayout>
-            </QueryClientProvider>
-          </Provider>
-        </ThemeProvider>
+        <RootLayoutWrapper>{children}</RootLayoutWrapper>
       </body>
     </html>
   );
