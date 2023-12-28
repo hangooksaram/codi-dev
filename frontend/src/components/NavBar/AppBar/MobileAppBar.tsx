1ff8dc433e6b2d0bd0ba307009b11bb4e3195b76
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-12-28 14:40:30 +0900

    FEAT:MobileAppBar 버튼 크기 수정

--- frontend/src/components/NavBar/AppBar/MobileAppBar.tsx
+++ frontend/src/components/NavBar/AppBar/MobileAppBar.tsx
@@ -21,6 +21,7 @@ const MobileAppBar = () => {
             const current = link === "/" ? path === "/" : path.includes(link);
             return (
               <MobileMenuButton
+                width="69px"
                 variant="default"
                 key={index}
                 onClick={() => router.push(link)}
@@ -66,7 +67,6 @@ const MobileAppBarContent = styled(Card)({
 
 const MobileMenuButton = styled(Button)(
   ({ selected }: { selected: boolean }) => ({
-    width: "69px",
     height: "67px",
     borderRadius: "20px",
     background: selected ? theme.colors.background : theme.colors.white,
