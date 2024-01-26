654a84497273059425c07ac8d47d49f3a0148e2a
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-01-26 10:14:12 +0900

    REFACTOR:useNewForm 에서 유효성 검증, 초기 데이터 페칭 기능 커스텀 훅으로 분리
    * 각각 useValidateForm, useInitializeForm

--- frontend/src/hooks/useNewForm/useNewForm.tsx
+++ frontend/src/hooks/useNewForm/useNewForm.tsx
@@ -1,32 +1,15 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 import { ValidType, ValidateConditions, invalid } from '../../utils/validate';
 import { getFormattedFormValues } from './utils';
+import useInitializeForm from './useInitializeForm';
+import useValidateForm from './useValidateForm';
 
 const useNewForm = (initialFormValues: FormType, serverData?: object) => {
   const [form, setForm] = useState<FormType>(
     getFormattedFormValues(initialFormValues),
   );
   const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const setFormFromServerData = (data: object) => {
-    const formValues = { ...form };
-    Object.keys(data).forEach((key) => {
-      if (Object.hasOwn(form, key)) {
-        Object.assign(formValues, {
-          ...formValues,
-          [key]: {
-            ...formValues[key],
-            value: data[key as keyof typeof data],
-          },
-        });
-      }
-    });
-    return formValues;
-  };
-
-  useEffect(() => {
-    if (serverData) setForm(setFormFromServerData(serverData));
-  }, [serverData]);
+  if (serverData) useInitializeForm(form, setForm, serverData);
 
   const convertToFormData = () => {
     const formValues = { ...form };
@@ -64,49 +47,14 @@ const useNewForm = (initialFormValues: FormType, serverData?: object) => {
     });
   };
 
-  const validateFormValue = <T,>(
-    formName: keyof typeof form,
-    formValue: FormPropertyType<T>,
-  ) => {
-    const isValid = setIsValid(formValue.value!, formValue.validCondition);
-    setForm((prevForm) => {
-      return {
-        ...prevForm,
-        [formName]: {
-          ...form[formName],
-          isValid,
-        } as FormPropertyType<T>,
-      };
-    });
-
-    return isValid;
-  };
-
-  const validateAllFormValues = () => {
-    const isValid: ValidType[] = [];
-    Object.entries(form).forEach((item) => {
-      const [formName, formValue] = item;
-      isValid.push(
-        validateFormValue<typeof formValue.value>(
-          formName as keyof typeof form,
-          formValue,
-        ),
-      );
-    });
-    setIsSubmitted(true);
-    console.log(form);
-
-    return !isValid.includes('invalid');
-  };
-
-  const setIsValid = <T,>(value: T, validCondition: ValidateConditions) => {
-    return invalid(value, validCondition) ? 'invalid' : 'valid';
-  };
+  const { validateAllFormValues, validateFormValue, setIsValid } =
+    useValidateForm(form, setForm, setIsSubmitted);
 
   return {
     form,
     handleFormValueChange,
     validateAllFormValues,
+    validateFormValue,
     setIsSubmitted,
     convertToFormData,
   };
