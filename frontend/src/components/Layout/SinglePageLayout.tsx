86fb71def003e810101a8f3d44e5dca2a01ccc5f
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-03-02 19:04:59 +0900

    FEAT:SinglePageLayout 반응형 디자인 추가

--- frontend/src/components/Layout/SinglePageLayout.tsx
+++ frontend/src/components/Layout/SinglePageLayout.tsx
@@ -3,20 +3,19 @@
 import styled from '@emotion/styled';
 import { ReactNode } from 'react';
 import theme, { device } from '@/ui/theme';
+import Container from '@/ui/atoms/Container';
 
-const Layout = styled.div(({ color }) => ({
+const Layout = styled.div(() => ({
   width: '100%',
-
   backgroundColor: theme.colors.background,
-  [device('tablet')]: {
-    height: 'calc(100vh - 59px)',
-  },
 }));
 
-const StyledSinglePageLayoutSinglePageLayout = styled.main(({}) => ({
-  width: '69%',
-  margin: '0 auto',
+const StyledSinglePageLayoutSinglePageLayout = styled(Container)(({}) => ({
   padding: '50px 0px',
+
+  [device('tablet')]: {
+    padding: '30px 0px',
+  },
 }));
 
 function SinglePageLayout({ children }: { children: ReactNode }) {
