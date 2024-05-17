7a0155e0f4433e4c197d1393de63d762ac1aac57
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-05-17 11:26:59 +0900

    feat:meta data title, description 내용 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,16 +5,21 @@ import GoogleAnalyticsWithLibrary from '@/components/GoogleAnalytics/GoogleAnaly
 import { Metadata } from 'next';
 
 const metaData: Metadata = {
-  title: 'CODI',
-  description: '장애인 1:1 멘토링 플랫폼',
+  title: '코디 - CODI : 장애인 1:1 멘토링 플랫폼',
+  description:
+    '같은 장애를 가진 멘토 에게 1:1 멘토링을 신청하여, 정보를 공유해보세요.',
   openGraph: {
-    title: 'CODI',
+    title: '코디 - CODI',
     description: '장애인 1:1 멘토링 플랫폼',
     url: 'https://www.codisabled.com/',
   },
 };
 
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: {
+  params: any;
+}): Promise<Metadata> {
   return metaData;
 }
 
